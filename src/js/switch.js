cfbb76d38958f01044f2b3d916fa50e56a84874b
Viktor Kalchenko <mfhillows@gmail.com>
2020-11-09 14:01:10 +0200

    Update switch.js

--- src/js/switch.js
+++ src/js/switch.js
@@ -7,37 +7,36 @@ const colors = [
   "#795548",
 ];
 
-const startButton = document.querySelector(".start");
-const stopButton = document.querySelector(".stop");
-
-const interval = {
-    idInterval: null,
-    isActive: false,
-    startSwitchColor() {
-        if (isActive) {
-            return;
-        }
-        this.isActive = true;
-        this.idInterval = setInterval(() => {
-          document.body.style.backgroundColor =
-            colors[randomIntegerFromInterval(0, colors.length)];
-        }, 1000);
-  },
-  stopSwitchColor() {
-    clearInterval(idInterval);
-    document.body.style.backgroundColor = colors[0];
-  },
-};
-
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
+const startButton = document.querySelector(".start");
+const stopButton = document.querySelector(".stop");
 
+const interval = {
+  idInterval: null,
+  isActive: false,
+  startSwitchColor() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    this.idInterval = setInterval(() => {
+      document.body.style.backgroundColor =
+        colors[randomIntegerFromInterval(0, colors.length)];
+    }, 1000);
+  },
+  stopSwitchColor() {
+    clearInterval(this.idInterval);
+    this.isActive = false;
+    document.body.style.backgroundColor = colors[0];
+  },
+};
 
-startButton.addEventListener("click", interval.startSwitchColor);
-stopButton.addEventListener("click", interval.stopSwitchColor);
-
-
-
-
+startButton.addEventListener("click", () => {
+  interval.startSwitchColor();
+});
+stopButton.addEventListener("click", () => {
+  interval.stopSwitchColor();
+});
